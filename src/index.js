1b5094d7114136d58fe23934ec4a83dd69226296
cmerchant <@cmerchant>
2019-08-20 17:12:10 -0400

    working on redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+
+  <Provider store={store} >
+    <App />
+  </Provider> , document.getElementById('root'));
 registerServiceWorker();
